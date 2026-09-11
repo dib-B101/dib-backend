866fc7266eab6d9badfab19cf4f50779b7f435ed
DESKTOP-MR7IIH9\SSAFY <tjalswhd1@naver.com>
2026-09-11 15:01:51 +0900

    feat(bookmark) : 북마크 조회 API 구현

--- src/main/java/com/b101/dib/product/query/controller/ProductQueryController.java
+++ src/main/java/com/b101/dib/product/query/controller/ProductQueryController.java
@@ -1,17 +1,13 @@
 package com.b101.dib.product.query.controller;
 
-import com.b101.dib.common.exception.BusinessException;
-import com.b101.dib.common.exception.ErrorCode;
 import com.b101.dib.product.query.dto.ProductQueryDto;
 import com.b101.dib.product.query.service.ProductQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
